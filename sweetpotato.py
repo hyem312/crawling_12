3465e62117999fef40cc560d070d353607127658
Choi-Bogyeong <101110562+Choi-Bogyeong@users.noreply.github.com>
2022-06-05 11:51:09 +0900

    Update sweetpotato.py

--- sweetpotato.py
+++ sweetpotato.py
@@ -1,3 +1,4 @@
+#student_1
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -40,3 +41,48 @@ for box in boxes:
 
 f.close()
 browser.close()
+
+#student_2
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+import time
+driver=webdriver.Chrome(executable_path="C:\chromedriver_win32\chromedriver.exe")
+url="https://shopping.naver.com/home/p/index.naver"
+driver.get(url)
+
+search=driver.find_element_by_xpath('//*[@id="_verticalGnbModule"]/div/div[2]/div/div[2]/div/div[2]/form/fieldset/div/input')
+search.click()
+driver.find_element_by_xpath('//*[@id="_verticalGnbModule"]/div/div[2]/div/div[2]/div/div[2]/form/fieldset/div/input').send_keys('감자')
+search.send_keys(Keys.ENTER)
+rank=driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div[2]/div[3]/div[1]/div[1]/ul/li[2]/a')
+rank.click()
+
+before_h = driver.execute_script("return window.scrollY")
+res=[]
+while True:
+    driver.find_element_by_css_selector("body").send_keys(Keys.END)
+    time.sleep(1)
+    after_h = driver.execute_script("return window.scrollY")
+
+    if after_h == before_h:
+        break
+    before_h=after_h
+items = driver.find_elements_by_css_selector(".basicList_info_area__17Xyo")
+
+for item in items:
+    name = item.find_element_by_css_selector(".basicList_title__3P9Q7").text
+    low=item.find_element_by_css_selector(".price_low__2vp2A").text
+    try:
+        price=item.find_element_by_css_selector(".price_num__2WUXn").text
+    except:
+        price="판매중단"
+    link=item.find_element_by_css_selector(".basicList_title__3P9Q7>a").get_attribute('href')
+    res.append((name, low+price, link))
+    print(name, low+price, link)
+
+import pandas as pd
+data=pd.DataFrame(res)
+data.to_csv('gamja.csv')
+
+driver.close()
